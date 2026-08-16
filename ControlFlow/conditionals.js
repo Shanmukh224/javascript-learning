8265b0ad2dc50c2c263b37f51ca2260faeb10dc0
chaintanya <c18360713@gmail.com>
2026-08-16 13:17:40 +0530

    Added Control Flow concepts

---
+++ ControlFlow/conditionals.js
@@ -0,0 +1,31 @@
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
+
+#ifelse
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade A+");
+} else if (marks >= 80) {
+    console.log("Grade A");
+} else if (marks >= 70) {
+    console.log("Grade B");
+} else if (marks >= 60) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
+
+let age = 20;
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+    console.log("You can drive");
+} else {
+    console.log("You cannot drive");
+}
---
+++ ControlFlow/loops.js
@@ -0,0 +1,32 @@
+//forloop
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+//Backwards
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
+//whileloop
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+//LoopThroughAnArray
+let fruits = ["Apple", "Banana", "Mango"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//forAndOfLoop
+let fruits = ["Apple", "Banana", "Mango"];
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
